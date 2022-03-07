19f95a8bc8e41bf2b17211a6f461844a1a6ddb64
Ariane Rocha <72419533+arianerfrancisco@users.noreply.github.com>
2022-03-07 17:49:01 -0300

    update projeto hibernate - intelij

---
+++ src/main/java/hibernate/Exemplo.java
@@ -0,0 +1,22 @@
+package hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Exemplo {
+    public static void main(String[] args) throws Exception {
+        Configuration cfg = new Configuration().configure();
+        SessionFactory sf = cfg.buildSessionFactory();
+
+        Session session = sf.getCurrentSession();
+
+        Produto p = new Produto();
+        p.setNome("p1");
+        session.beginTransaction();
+        session.save(p);
+        session.getTransaction().commit();
+
+        System.out.println(p.getId());
+    }
+}
